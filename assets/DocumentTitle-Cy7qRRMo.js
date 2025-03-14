f19c2f5253e3b1c42f4cc074184e9e28f737351f
d2184 <d2184@users.noreply.github.com>
2025-03-14 08:02:46 +0000

    deploy: afb98bc989ff65426609cba050bb6e385bba5bfd

---
+++ assets/DocumentTitle-Cy7qRRMo.js
@@ -0,0 +1,1 @@
+import{d as t,bw as r}from"./index-DUyAht6V.js";const o=({children:e})=>t(r,{get children(){return[e," - MetaCubeXD"]}});export{o as D};
---
+++ assets/time-DWiAe1fR.js
@@ -0,0 +1,1 @@
+import{V as a,b3 as m}from"./index-DUyAht6V.js";const s=o=>a(o).locale(m()).fromNow();export{s as f};
