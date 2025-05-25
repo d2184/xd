4825da9012c16d7f05145b7e3eaa5a88d1858ddf
d2184 <d2184@users.noreply.github.com>
2025-05-25 06:42:38 +0000

    deploy: 1f048cedf5505b9bcef20ac8988ef40460e3fe67

---
+++ assets/DocumentTitle-CnTkwovt.js
@@ -0,0 +1,1 @@
+import{d as t,bx as r}from"./index-C9bD-tnx.js";const o=({children:e})=>t(r,{get children(){return[e," - MetaCubeXD"]}});export{o as D};
---
+++ assets/time-DIDHgR_x.js
@@ -0,0 +1,1 @@
+import{W as a,b4 as m}from"./index-C9bD-tnx.js";const s=o=>a(o).locale(m()).fromNow();export{s as f};
